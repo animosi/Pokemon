c1bf957d4e4fbfee41c330d779f39ec4b01e0756
michael <animosus@gmail.com>
2020-08-08 05:27:36 +0700

    refactor + check async/timing

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 require('./src/config/database');
-const getIds = require('./src/services/getIds');
-const getPokemon = require('./src/services/getPokemon');
+const service = require('./src/services/getPokemon');
 
 const app = express();
+
+const tick = Date.now();
+const log = (v) => console.log(`${v} \n Elapsed: ${Date.now() - tick}ms`);
 
 app.listen(3000, () => console.log('connected on :3000'));
 
 //*  MAIN()  *//
 
-// getIds();
-getPokemon();
+log('async 1');
+(async () => {
+  await service.getIds().then(log);
+  service.getPokemon().then(log);
+})();
+log('async 2');
--- src/services/getPokemon.js
+++ src/services/getPokemon.js
@@ -2,23 +2,37 @@ const axios = require('axios');
 const PokemonModel = require('../models/pokemon');
 const PokemonIdsModel = require('../models/pokemonIds');
 
-const getPokemon = async () => {
-  try {
-    const ids = await PokemonIdsModel.find({});
+const limit = 10;
+const uri = `https://pokeapi.co/api/v2/pokemon`;
 
-    const array = await Promise.all(
-      ids.map(async (id) => {
-        const result = await axios.get(
-          `https://pokeapi.co/api/v2/pokemon/${id.name}`
-        );
-        const pokemon = new PokemonModel({ ...result.data });
-        return pokemon;
-      })
-    );
-    await PokemonModel.insertMany(array), (err) => console.log(err);
-  } catch (err) {
-    console.log('getPokemon() failed', err);
-  }
+module.exports = {
+  getIds: async () => {
+    try {
+      const data = await axios.get(uri, { params: { limit } });
+      await PokemonIdsModel.insertMany(data.data.results),
+        (err) => console.log(err);
+    } catch (err) {
+      console.log(err);
+    }
+    return 'Get ids complete';
+  },
+  getPokemon: async () => {
+    try {
+      const ids = await PokemonIdsModel.find({});
+
+      const array = await Promise.all(
+        ids.map(async (id) => {
+          const result = await axios.get(
+            `https://pokeapi.co/api/v2/pokemon/${id.name}`
+          );
+          const pokemon = new PokemonModel({ ...result.data });
+          return pokemon;
+        })
+      );
+      await PokemonModel.insertMany(array), (err) => console.log(err);
+    } catch (err) {
+      console.log('getPokemon() failed', err);
+    }
+    return 'Get pokemon complete';
+  },
 };
-
-module.exports = getPokemon;
